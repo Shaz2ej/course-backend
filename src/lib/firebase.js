15e4adfd077a8a98c25b90326bc25f43cb495c5e
Shaz2ej <youthpargatii@gmail.com>
2025-10-28 02:21:35 -0700

    Add comment to firebase.js to trigger new Netlify build

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,5 @@
 // src/lib/firebase.js
+// This file is required for Vite build process
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
