e1c909a4927f9de67b26cbf238083417196cca69
Shaz2ej <youthpargatii@gmail.com>
2025-10-28 03:58:28 -0700

    Add placeholder firebase.js file to resolve Netlify build error

---
+++ src/lib/firebase.js
@@ -0,0 +1,6 @@
+// Placeholder file to resolve Netlify build error
+// This file is not used in the application but is required to prevent import errors
+
+export const auth = null;
+export const db = null;
+export default null;
